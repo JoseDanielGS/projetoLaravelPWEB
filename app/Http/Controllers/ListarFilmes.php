bce7eecd86a42d4e05c7ecdae5e71b22376554c2
José Daniel Gomes dos Santos <j.g.santos.1998@gmail.com>
2022-07-14 15:40:29 -0300

    adicionando view simples

--- app/Http/Controllers/ListarFilmes.php
+++ app/Http/Controllers/ListarFilmes.php
@@ -1,20 +1,21 @@
 <?php
-
+//controller criado com 'php artisan make:conttroler'
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
 class ListarFilmes extends Controller
-{
+{   // há um padrão em ... que indica os nomes dos métodos 
     public function index(Request $requestgit){
         $filmes = ['A Vida é Bela','A Lista de Schindler','The Shosen'];
 
-        $html = '<ul>';
-        foreach($filmes as $filme){
-            $html.= "<li>$filme</li>";
-        }
-        $html.='</lu>';
-
-        return $html;
+        //$html = '<ul>';
+        //foreach($filmes as $filme){
+        //    $html.= "<li>$filme</li>";
+        //}
+        //$html.='</lu>';
+        
+        //o Laravel transforma a String de retorno na resposta do request, se for algum objeto, transforma em JSON
+        return view('listar-filmes', compact(var_name:'filmes'));
     }
 }
---
+++ resources/views/listar-filmes.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+ <head></head>
+
+ <body>
+    <lu>
+    <?php foreach ($filmes as $filme): ?>
+        <li><?=$filme; ?></li>
+    <?php endforeach;  ?>
+    </lu>
+ </body>
+
+</html>
