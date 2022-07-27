3d3d3ef7f27e1131a9f89e07213c92b5030f878f
José Daniel <j.g.santos.1998@gmail.com>
2022-07-27 16:30:13 -0300

    pequena limpeza no controller

--- app/Http/Controllers/RequisicoesController.php
+++ app/Http/Controllers/RequisicoesController.php
@@ -10,15 +10,7 @@ class RequisicoesController extends Controller
 {   // há um padrão em ... que indica os nomes dos métodos 
     public function index(){
         $requisicoes = Requisicao::query()->orderBy('material')->get();
-        //dd($requisicoes);
-
-        //$html = '<ul>';
-        //foreach($requisicoes as $filme){
-        //    $html.= "<li>$filme</li>";
-        //}
-        //$html.='</lu>';
-        
-        //o Laravel transforma a String de retorno na resposta do request, se for algum objeto, transforma em JSON
+      
         return view('requisicoes.index')->with('requisicoes',$requisicoes);
     }
 
@@ -29,36 +21,19 @@ class RequisicoesController extends Controller
 
     public function store(Request $request){
         
-        //dd($request->all());
-        //$request->all()->status = "conluido";
+      
         Requisicao::create($request->all());
         return to_route('requisicoes.index');
-        
-        /*
-        $nomeSerie = $requestgit->input('nome');
-        $requisicoes = new Requisicao();
-        $requisicoes->name = $nomeSerie;
-        $requisicoes->save();
-        return redirect("/requisicoes");
-        */
-
+      
     }
 
     public function edit(Request $requestgit){
         return view('requisicoes.edit')->with('movie',Requisicao::find($requestgit->movie));
-        
-        /*
-        if(DB::insert('insert into requisicoes (nome) values (?);', [$nomeSerie])){
-            return 'OK';
-        }else{
-            return 'error';
-        }*/
 
     }
 
     public function update(Request $requestgit){
         $requisicoes = Requisicao::find($requestgit->movie);
-        // $filme = Requisicao::find($requestgit->get('id'));
          $requisicoes->name = $requestgit->input('NovoNome');
          $requisicoes->save();
          return to_route('requisicoes.index');
@@ -68,19 +43,6 @@ class RequisicoesController extends Controller
         
         Requisicao::destroy($request->movie);
         return to_route('requisicoes.index');
-
-
-       // $requisicoes = Requisicao::find($requestgit->get('id'));
-       // $filme = Requisicao::find($requestgit->get('id'));
-       // $requisicoes->delete();
-       //return redirect("/requisicoes");
-        
-        /*
-        if(DB::insert('insert into requisicoes (nome) values (?);', [$nomeSerie])){
-            return 'OK';
-        }else{
-            return 'error';
-        }*/
 
     }
 }
