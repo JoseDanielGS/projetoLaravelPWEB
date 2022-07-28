ee08520daa9fd1adbbc4dbadd9890279cc0a35a7
José Daniel <j.g.santos.1998@gmail.com>
2022-07-28 16:47:43 -0300

    rotas update e destroy adicionadas

--- app/Http/Controllers/RequisicoesControllerAPI.php
+++ app/Http/Controllers/RequisicoesControllerAPI.php
@@ -14,4 +14,17 @@ class RequisicoesControllerAPI extends Controller
     public function store(Request $request){
         return response()->json(Requisicao::create($request->all()), 201);
     }
+
+    public function update(Request $request){
+        $requisicao = Requisicao::find($request->all());
+        $requisicao->save();
+        return $requisicao;
+    }
+
+    public function destroy(int $id){
+        Requisicao::destroy($id);
+        return response()->noContent();
+    }
+
+
 }
